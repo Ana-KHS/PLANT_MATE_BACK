53968614985e9c140e24f3bdcb233e0bcfeb29ac
Ana-KHS <ana.hskwon@gmail.com>
2023-06-06 23:18:12 +0900

    initial commit

---
+++ PLANT_MATE/src/main/java/com/sqisoft/plant_mate/PlantMateApplication.java
@@ -0,0 +1,13 @@
+package com.sqisoft.plant_mate;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PlantMateApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(PlantMateApplication.class, args);
+	}
+
+}
---
+++ PLANT_MATE/src/test/java/com/sqisoft/plant_mate/PlantMateApplicationTests.java
@@ -0,0 +1,13 @@
+package com.sqisoft.plant_mate;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PlantMateApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
