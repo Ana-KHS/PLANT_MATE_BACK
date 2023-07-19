8921e533b7fcd8a73d79cd8f4f4be1dc1247155f
June Young Ahn <jynius@gmail.com>
2023-07-19 10:33:59 +0900

    로그아웃하려고 했는데...

--- PlantMateServer/src/main/java/com/sqisoft/plantmate/SecurityConfig.java
+++ PlantMateServer/src/main/java/com/sqisoft/plantmate/SecurityConfig.java
@@ -46,7 +46,7 @@ public class SecurityConfig {
 			"/v3/api-docs/**", "/swagger-ui/**", "/swagger-ui.html",
 			"/api/auth/**", "/api/user", "/api/user/**",
 			"/api/search/**",
-//			"/api/community/**"			
+			"/api/file/*"			
 	};
 	
 	private final UserDetailsService userDetailsService;
